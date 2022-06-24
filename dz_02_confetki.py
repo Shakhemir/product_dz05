33b02b88e8cda17ef0ac156551421bc083a724ef
Shakhemir <shakhemir@gmail.com>
2022-06-24 16:57:25 +0300

    Улучшения: в конфетках убрал лишнее, в RLE добавил аргументы функциям

--- dz_02_confetki.py
+++ dz_02_confetki.py
@@ -4,7 +4,7 @@
 
 from random import randint
 
-common_count = 2021
+common_count = 200
 max_take = 28
 input("Нажмите Enter для того, чтобы выбрать кто ходит первый")
 select = randint(1, 7)
@@ -13,16 +13,12 @@ comp_select = randint(1, 7)
 print("Компьютеру выпало:", comp_select)
 turn_player = False if comp_select > select else True
 while common_count > max_take:
-    if common_count <= max_take:
-        max_count = common_count - 1
-    else:
-        max_count = max_take
     if turn_player:
         confets = max_take + 1
-        while confets > max_count:
-            confets = int(input(f"Ваш ход. Сколько конфеток берете (от 1 до {max_count})? "))
+        while confets > max_take:
+            confets = int(input(f"Ваш ход. Сколько конфеток берете (от 1 до {max_take})? "))
     else:
-        confets = randint(1, max_count + 1)
+        confets = randint(1, max_take + 1)
         print(f"Компьютер взял {confets} конфет")
     common_count -= confets
     print(f"Осталось {common_count} конфет")
--- dz_04_rle.py
+++ dz_04_rle.py
@@ -1,33 +1,39 @@
 """ Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных """
 
-in_file = 'rle_encode.txt'
-out_file = 'rle_decode.txt'
+encode_in_file = 'rle_encode_in.txt'
+encode_out_file = 'rle_encode_out.txt'
+decode_in_file = 'rle_decode_in.txt'
+decode_out_file = 'rle_decode_out.txt'
 
 
-def encode_file():
+def encode_file(in_file=encode_in_file, out_file=encode_out_file):
+    print('Кодирование файла', in_file)
     with open(in_file, 'r') as file:
         txt = file.read().strip()
+    print('Входящие данные:')
     print(txt)
-
     index, index2 = 0, 0
     encoded_txt = ''
     while index < len(txt):
         encoded_txt += txt[index]
         count = 0
-        while index2 < len(txt):
-            if txt[index2] == txt[index]:
-                index2 += 1
-                count += 1
-            else:
-                break
+        while index2 < len(txt) and txt[index2] == txt[index]:
+            index2 += 1
+            count += 1
         index = index2
         encoded_txt += str(count)
+    if out_file:
+        with open(out_file, 'w') as file:
+            print(encoded_txt, file=file)
+    print('Исходящие данные:')
     print(encoded_txt)
 
 
-def decode_file():
-    with open(out_file, 'r') as file:
+def decode_file(in_file=decode_in_file, out_file=decode_out_file):
+    print('Декодирование файла', in_file)
+    with open(in_file, 'r') as file:
         txt = file.readline().strip()
+    print('Входящие данные:')
     print(txt)
     decoded_txt = ''
     index = 0
@@ -37,12 +43,14 @@ def decode_file():
             index2 += 1
         decoded_txt += txt[index] * int(txt[index + 1:index2])
         index = index2
+    if out_file:
+        with open(out_file, 'w') as file:
+            print(decoded_txt, file=file)
+    print('Исходящие данные:')
     print(decoded_txt)
 
 
 if __name__ == '__main__':
-    print('Кодирование файла', in_file)
     encode_file()
-    print()
-    print('Декодирование файла', out_file)
+    print('-' * 50)
     decode_file()
